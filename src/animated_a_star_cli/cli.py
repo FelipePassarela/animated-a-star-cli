7a1459b4e63e49dd710cbc23fdb4b2284c9ee7b3
FelipePassarela <126916744+FelipePassarela@users.noreply.github.com>
2026-05-03 03:38:12 -0300

    style: :safety_vest: improve error messages

--- src/animated_a_star_cli/cli.py
+++ src/animated_a_star_cli/cli.py
@@ -12,18 +12,21 @@ def main():
 
 
 def _load_config() -> config.Config:  # ty:ignore[invalid-return-type]
+    error_prefix = "Error loading config:"
+
     try:
         return parser.load_config()
     except FileNotFoundError:
-        _print_error_and_exit("Config file not found.", exit_code=3)
+        _print_error_and_exit(f"{error_prefix}: Config file not found.", exit_code=3)
     except parser.ParserError as e:
         error_msg = (
-            f"Error loading config: {str(e).capitalize()}. "
-            "Please check your config file."
+            f"{error_prefix} {str(e).capitalize()}. Please check your config file."
         )
         _print_error_and_exit(error_msg, exit_code=2)
     except Exception as e:
-        _print_error_and_exit(f"Unexpected error: {e}", exit_code=1)
+        _print_error_and_exit(
+            f"Unexpected error while loading config: {e}", exit_code=1
+        )
 
 
 def _print_error_and_exit(message: str, exit_code: int = 1):
