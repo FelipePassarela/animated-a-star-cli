09522a7aabb94c9f3af13beda3dd7c954b4f6ba4
FelipePassarela <126916744+FelipePassarela@users.noreply.github.com>
2026-05-03 17:38:53 -0300

    feat(render): :sparkles: integrate AStarState into rendering logic
    Added AStarState to the RenderContext and updated the draw function to visualize closed cells and paths. Enhanced tests to cover rendering scenarios for closed cells and paths, ensuring correct display of the A* algorithm's state.

--- src/animated_a_star_cli/ui/render.py
+++ src/animated_a_star_cli/ui/render.py
@@ -3,10 +3,20 @@ from animated_a_star_cli.ui.render_context import RenderContext
 
 def draw(ctx: RenderContext):
     config = ctx.cfg
+    grid = config.map.grid.copy()
 
-    grid = config.map.grid
+    if ctx.astar_state is not None:
+        closed = ctx.astar_state.closed_cells
+        path = ctx.astar_state.path
+        if closed:
+            rows, cols = zip(*closed)
+            grid[rows, cols] = "."
+        if path:
+            rows, cols = zip(*path)
+            grid[rows, cols] = "*"
+
     grid[config.source] = "o"
     grid[config.dest] = "x"
 
-    sprite = "\n".join("".join(row) for row in grid) + "\n"
+    sprite = "\n".join("".join(row) for row in grid)
     print(sprite)
--- tests/unit/test_render.py
+++ tests/unit/test_render.py
@@ -1,33 +1,136 @@
+import textwrap
+
 import pytest
 
+from animated_a_star_cli.core.astar_state import AStarState
 from animated_a_star_cli.core.config import Config
 from animated_a_star_cli.core.map import Map
 from animated_a_star_cli.ui.render import draw
 from animated_a_star_cli.ui.render_context import RenderContext
 
+grid = [
+    "######",
+    "#o   #",
+    "###  #",
+    "#x ###",
+    "######",
+]
+
+
+@pytest.fixture
+def render_ctx():
+    map_obj = Map(grid)
+    config = Config(map=map_obj, source=(1, 1), dest=(3, 1))
+    return RenderContext(cfg=config)
+
 
 def test_draw_renders_map_with_source_and_destination(
-    capsys: pytest.CaptureFixture[str],
+    capsys: pytest.CaptureFixture[str], render_ctx: RenderContext
 ):
-    grid = [
-        "#####",
-        "#   #",
-        "# # #",
-        "#####",
-    ]
-
-    map_obj = Map(grid)
-    config = Config(map=map_obj, source=(1, 1), dest=(2, 3))
-    ctx = RenderContext(cfg=config)
-
-    draw(ctx)
+    draw(render_ctx)
 
     captured = capsys.readouterr()
-    expected = (
-        "#####\n"
-        "#o  #\n"
-        "# #x#\n"
-        "#####\n"
-        "\n"  # print() adds an extra newline
+    expected = textwrap.dedent("""\
+        ######
+        #o   #
+        ###  #
+        #x ###
+        ######
+    """)
+    assert captured.out == expected
+
+
+def test_draw_renders_closed_cells(
+    capsys: pytest.CaptureFixture[str], render_ctx: RenderContext
+):
+    render_ctx.astar_state = AStarState(closed_cells={(1, 4), (2, 3)})
+    draw(render_ctx)
+
+    captured = capsys.readouterr()
+    expected = textwrap.dedent("""\
+        ######
+        #o  .#
+        ###. #
+        #x ###
+        ######
+    """)
+    assert captured.out == expected
+
+
+def test_draw_renders_path(
+    capsys: pytest.CaptureFixture[str], render_ctx: RenderContext
+):
+    render_ctx.astar_state = AStarState(path=[(1, 2), (1, 3), (2, 3), (3, 2)])
+
+    draw(render_ctx)
+
+    captured = capsys.readouterr()
+    expected = textwrap.dedent("""\
+        ######
+        #o** #
+        ###* #
+        #x*###
+        ######
+    """)
+    assert captured.out == expected
+
+
+def test_path_cells_take_precedence_over_closed_cells(
+    capsys: pytest.CaptureFixture[str], render_ctx: RenderContext
+):
+    render_ctx.astar_state = AStarState(
+        closed_cells={(1, 2), (1, 3), (2, 3)},
+        path=[(1, 2), (1, 3), (2, 3)],
     )
+
+    draw(render_ctx)
+
+    captured = capsys.readouterr()
+    expected = textwrap.dedent("""\
+        ######
+        #o** #
+        ###* #
+        #x ###
+        ######
+    """)
+    assert captured.out == expected
+
+
+def test_source_and_dest_dont_get_overriden(
+    capsys: pytest.CaptureFixture[str], render_ctx: RenderContext
+):
+    render_ctx.cfg.source = (1, 1)
+    render_ctx.cfg.dest = (3, 1)
+    render_ctx.astar_state = AStarState(
+        closed_cells={(1, 1), (3, 1)},
+        path=[(1, 1), (3, 1)],
+    )
+
+    draw(render_ctx)
+
+    captured = capsys.readouterr()
+    expected = textwrap.dedent("""\
+        ######
+        #o   #
+        ###  #
+        #x ###
+        ######
+    """)
+    assert captured.out == expected
+
+
+def test_draw_succeeds_with_none_astar_state(
+    capsys: pytest.CaptureFixture[str], render_ctx: RenderContext
+):
+    render_ctx.astar_state = None
+    draw(render_ctx)
+
+    captured = capsys.readouterr()
+    expected = textwrap.dedent("""\
+        ######
+        #o   #
+        ###  #
+        #x ###
+        ######
+    """)
     assert captured.out == expected
