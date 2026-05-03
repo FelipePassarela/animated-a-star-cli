703a3921bbbd0d4b239ffdff114fd360ae8cf71b
FelipePassarela <126916744+FelipePassarela@users.noreply.github.com>
2026-05-03 00:46:54 -0300

    feat(cli): refactor config loading and map parsing and print map
    Refactor the main function to load configuration and parse the map
    more cleanly. Improved error handling by utilizing a dedicated
    _print_error_and_exit function for consistent error messaging.

--- src/animated_a_star_cli/cli.py
+++ src/animated_a_star_cli/cli.py
@@ -1,20 +1,37 @@
 import sys
 
+from animated_a_star_cli.ui import parser
+
 from . import config
 
 
 def main():
+    cfg = _load_config()
+    parsed_map = _parse_map(cfg.map)
+    print(parsed_map)
+
+
+def _load_config() -> config.Config:  # ty:ignore[invalid-return-type]
     try:
-        cfg = config.load_config()
-        print("Loaded config:")
-        print(cfg)
+        return config.load_config()
     except FileNotFoundError:
-        print("Config file not found.")
-        sys.exit(3)
+        _print_error_and_exit("Config file not found.", exit_code=3)
     except config.ConfigError as e:
         error_msg = f"{str(e).capitalize()}. Please check your config file."
-        print(f"Error loading config: {error_msg}")
-        sys.exit(2)
+        _print_error_and_exit(f"Error loading config: {error_msg}", exit_code=2)
     except Exception as e:
-        print(f"Unexpected error: {e}")
-        sys.exit(1)
+        _print_error_and_exit(f"Unexpected error: {e}", exit_code=1)
+
+
+def _parse_map(map_str: str):
+    try:
+        return parser.parse_map(map_str)
+    except parser.ParserError as e:
+        _print_error_and_exit(f"Error parsing map: {e}", exit_code=4)
+    except Exception as e:
+        _print_error_and_exit(f"Unexpected error: {e}", exit_code=1)
+
+
+def _print_error_and_exit(message: str, exit_code: int = 1):
+    print(message)
+    sys.exit(exit_code)
