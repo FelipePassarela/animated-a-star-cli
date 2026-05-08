725f38203381ddc5ce2251a634a8fe14d198f710
FelipePassarela <126916744+FelipePassarela@users.noreply.github.com>
2026-05-08 16:37:22 -0300

    feat(render): :sparkles: add draw_to_string function to facilitate render tests

--- src/animated_a_star_cli/ui/render.py
+++ src/animated_a_star_cli/ui/render.py
@@ -5,26 +5,29 @@ from animated_a_star_cli.ui.render_context import RenderContext
 
 
 def draw(ctx: RenderContext):
+    print(draw_to_string(ctx))
+
+
+def draw_to_string(ctx: RenderContext) -> str:
     config = ctx.cfg
-    grid = config.map.grid.copy()
+    map_grid = config.map.grid.copy()
 
     if ctx.astar_state is not None:
         closed = ctx.astar_state.closed_cells
         path = ctx.astar_state.path
         if closed:
             rows, cols = zip(*closed)
-            grid[rows, cols] = "."
+            map_grid[rows, cols] = "."
         if path:
             rows, cols = zip(*path)
-            grid[rows, cols] = "*"
+            map_grid[rows, cols] = "*"
 
-    grid[config.source] = "o"
-    grid[config.dest] = "x"
+    map_grid[config.source] = "o"
+    map_grid[config.dest] = "x"
 
-    sprite = "\n".join("".join(row) for row in grid) + "\n"
+    sprite = "\n".join("".join(row) for row in map_grid) + "\n"
     sprite += _heuristic_to_string(config.heuristic)
-
-    print(sprite)
+    return sprite
 
 
 def _heuristic_to_string(h: Callable) -> str:
--- tests/unit/test_render.py
+++ tests/unit/test_render.py
@@ -7,7 +7,7 @@ from animated_a_star_cli.core.astar_state import AStarState
 from animated_a_star_cli.core.config import Config
 from animated_a_star_cli.core.heuristic import euclidean, manhattan
 from animated_a_star_cli.core.map import Map
-from animated_a_star_cli.ui.render import draw
+from animated_a_star_cli.ui.render import draw_to_string
 from animated_a_star_cli.ui.render_context import RenderContext
 
 grid = [
@@ -23,21 +23,18 @@ BASE_MAP_OUTPUT = textwrap.dedent("""\
     #o   #
     ###  #
     #x ###
-    ######
-""")
+    ######""")
 
 
 def draw_and_assert(
-    capsys: pytest.CaptureFixture[str],
     render_ctx: RenderContext,
     *,
     expected_map_output: str = BASE_MAP_OUTPUT,
     expected_heuristic: str = "Euclidean",
 ):
-    draw(render_ctx)
-    captured = capsys.readouterr()
-    expected = expected_map_output + f"Heuristic: {expected_heuristic}\n"
-    assert captured.out == expected
+    output = draw_to_string(render_ctx)
+    expected = f"{expected_map_output}\nHeuristic: {expected_heuristic}"
+    assert output == expected
 
 
 @pytest.fixture
@@ -51,10 +48,8 @@ def render_ctx() -> RenderContext:
 
 class TestStaticRendering:
     @staticmethod
-    def test_draw_renders_map_with_source_and_destination(
-        capsys: pytest.CaptureFixture[str], render_ctx: RenderContext
-    ):
-        draw_and_assert(capsys, render_ctx)
+    def test_draw_renders_map_with_source_and_destination(render_ctx: RenderContext):
+        draw_and_assert(render_ctx)
 
     @pytest.mark.parametrize(
         "heuristic, expected_heuristic",
@@ -66,13 +61,12 @@ class TestStaticRendering:
     )
     @staticmethod
     def test_draw_renders_correct_heuristic_name(
-        capsys: pytest.CaptureFixture[str],
         render_ctx: RenderContext,
         heuristic: Callable,
         expected_heuristic: str,
     ):
         render_ctx.cfg.heuristic = heuristic
-        draw_and_assert(capsys, render_ctx, expected_heuristic=expected_heuristic)
+        draw_and_assert(render_ctx, expected_heuristic=expected_heuristic)
 
 
 class TestPathRendering:
@@ -81,60 +75,46 @@ class TestPathRendering:
         #o** #
         ###* #
         #x*###
-        ######
-    """)
+        ######""")
     EXPECTED_CLOSEDS_CELLS_OUTPUT = textwrap.dedent("""\
         ######
         #o  .#
         ###. #
         #x ###
-        ######
-    """)
+        ######""")
     PATH_CELLS = [(1, 2), (1, 3), (2, 3), (3, 2)]
 
-    def test_draw_renders_path(
-        self, capsys: pytest.CaptureFixture[str], render_ctx: RenderContext
-    ):
+    def test_draw_renders_path(self, render_ctx: RenderContext):
         render_ctx.astar_state = AStarState(path=self.PATH_CELLS)
-        draw_and_assert(
-            capsys, render_ctx, expected_map_output=self.EXPECTED_PATH_OUTPUT
-        )
+        draw_and_assert(render_ctx, expected_map_output=self.EXPECTED_PATH_OUTPUT)
 
-    def test_draw_renders_closed_cells(
-        self, capsys: pytest.CaptureFixture[str], render_ctx: RenderContext
-    ):
+    def test_draw_renders_closed_cells(self, render_ctx: RenderContext):
         render_ctx.astar_state = AStarState(closed_cells={(1, 4), (2, 3)})
         draw_and_assert(
-            capsys, render_ctx, expected_map_output=self.EXPECTED_CLOSEDS_CELLS_OUTPUT
+            render_ctx, expected_map_output=self.EXPECTED_CLOSEDS_CELLS_OUTPUT
         )
 
     def test_path_cells_take_precedence_over_closed_cells(
-        self, capsys: pytest.CaptureFixture[str], render_ctx: RenderContext
+        self, render_ctx: RenderContext
     ):
         render_ctx.astar_state = AStarState(
             closed_cells=set(self.PATH_CELLS),
             path=self.PATH_CELLS,
         )
-        draw_and_assert(
-            capsys, render_ctx, expected_map_output=self.EXPECTED_PATH_OUTPUT
-        )
+        draw_and_assert(render_ctx, expected_map_output=self.EXPECTED_PATH_OUTPUT)
 
-    def test_source_and_dest_dont_get_overriden(
-        self, capsys: pytest.CaptureFixture[str], render_ctx: RenderContext
-    ):
+    def test_source_and_dest_dont_get_overriden(self, render_ctx: RenderContext):
         render_ctx.cfg.source = (1, 1)
         render_ctx.cfg.dest = (3, 1)
         render_ctx.astar_state = AStarState(
             closed_cells={(1, 1), (3, 1)},
             path=[(1, 1), (3, 1)],
         )
-        draw_and_assert(capsys, render_ctx)
+        draw_and_assert(render_ctx)
 
 
 class TestEdgeCases:
     @staticmethod
-    def test_draw_succeeds_with_none_astar_state(
-        capsys: pytest.CaptureFixture[str], render_ctx: RenderContext
-    ):
+    def test_draw_succeeds_with_none_astar_state(render_ctx: RenderContext):
         render_ctx.astar_state = None
-        draw_and_assert(capsys, render_ctx)
+        draw_and_assert(render_ctx)
